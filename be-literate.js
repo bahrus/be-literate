68d9057f6e898bb62ea2bbcdc08a9a17ff4a74bb
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-27 20:42:23 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-literate into baseline

---
+++ be-literate.js
@@ -0,0 +1,5 @@
+// @ts-check
+import { resolved, rejected, propInfo} from 'be-enhanced/cc.js';
+import { BE } from 'be-enhanced/BE.js';
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-bound/types.d.ts' */;
