127ff2de70a0c0ef6cfe06507a7274a9809edba1
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-27 21:14:41 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-literate into baseline

--- be-literate.js
+++ be-literate.js
@@ -2,4 +2,20 @@
 import { resolved, rejected, propInfo} from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-bound/types.d.ts' */;
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-literate/types.d.ts' */;
+
+/**
+ * @implements {Actions}
+ * 
+ */
+class BeLiterate extends BE {
+    /**
+     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement, any>}
+     */
+    static config = {
+
+    }
+}
+
+await BeLiterate.bootUp();
+export { BeLiterate }
